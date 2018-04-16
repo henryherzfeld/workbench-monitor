65d9f58b070ff40e164fd1f7c1e1c0f7a47723f4
HenryHerzfeld <herzfeld2@gmail.com>
2018-04-16 14:50:10 -0400

    more restructuring for assets

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -22,8 +22,7 @@
 <script src="{{ asset('assets/js/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('assets/js/dataTables.bootstrap4.js') }}"></script>
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('assets/js/sb-admin.min.js') }}"></script>
-<!-- Custom scripts for this page-->
+<script src="{{ asset('assets/js/sb-admin.js') }}"></script>
 <script src="{{ asset('assets/js/sb-admin-datatables.min.js') }}"></script>
 <script src="{{ asset('assets/js/sb-admin-charts.min.js') }}"></script>
 
