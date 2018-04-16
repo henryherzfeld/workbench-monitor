b17dcf293625876569430e08e8aee3c352833182
HenryHerzfeld <herzfeld2@gmail.com>
2018-04-16 15:01:51 -0400

    cleaned up template for seperation between itself and content

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,23 +11,6 @@
       @endif
 
     @section('content')
-        <div class="col-xl-3 col-sm-6 mb-3">
-          <div class="card text-white bg-info o-hidden h-100">
-            <div class="card-body">
-              <div class="card-body-icon">
-                <i class="fa fa-fw fa-support"></i>
-              </div>
-              <div class="mr-5">Current build supports tables view (on left) only!</div>
-            </div>
-            <a class="card-footer text-white clearfix small z-1" href="{{ route('tables') }}">
-              <span class="float-left">View Details </span>
-              <span class="float-right">
-                <i class="fa fa-angle-right"></i>
-              </span>
-            </a>
-          </div>
-        </div>
-      </div>
       <!-- Area Chart Example-->
       <div class="card mb-3">
         <div class="card-header">
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,10 +26,11 @@
                 <h1>@yield('title')</h1>
             </div>
         </div>
-    </div>
-</div>
 
 @yield('content')
+
+    </div>
+</div>
 
 @include('layouts.footer')
 
